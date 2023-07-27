37e6ee0f0c186883d59ecffe86bfe089e429dd3a
Pavel Zarecky <zarecky@procivis.ch>
2023-07-27 15:54:04 +0200

    ONE-459: fixed typescript build

---
+++ index.ts
@@ -0,0 +1,81 @@
+import { NativeModules } from "react-native";
+
+export interface Version {
+  target: string;
+  buildTime: string;
+  branch: string;
+  tag: string;
+  commit: string;
+  rustVersion: string;
+  pipelineId: string;
+}
+
+export interface InvitationResult {
+  issuedCredentialId: string;
+}
+
+export enum CredentialState {
+  CREATED = "CREATED",
+  PENDING = "PENDING",
+  OFFERED = "OFFERED",
+  ACCEPTED = "ACCEPTED",
+  REJECTED = "REJECTED",
+  REVOKED = "REVOKED",
+  ERROR = "ERROR",
+}
+
+export enum RevocationMethod {
+  NONE = "NONE",
+  STATUS_LIST2021 = "STATUS_LIST2021",
+  LVVC = "LVVC",
+}
+
+export enum CredentialFormat {
+  JWT = "JWT",
+  SD_JWT = "SD_JWT",
+  JSON_LD = "JSON_LD",
+  MDOC = "MDOC",
+}
+
+export enum ClaimDataType {
+  STRING = "STRING",
+  DATE = "DATE",
+  NUMBER = "NUMBER",
+}
+
+export interface CredentialSchema {
+  id: string;
+  createdDate: string;
+  lastModified: string;
+  name: string;
+  organisationId: string;
+  format: CredentialFormat;
+  revocationMethod: RevocationMethod;
+}
+
+export interface Claim {
+  id: string;
+  key: string;
+  dataType: ClaimDataType;
+  value: string;
+}
+
+export interface Credential {
+  id: string;
+  createdDate: string;
+  issuanceDate: string;
+  lastModified: string;
+  issuerDid?: string | null;
+  state: CredentialState;
+  claims: Claim[];
+  schema: CredentialSchema;
+}
+
+interface ONECore {
+  handleInvitation(url: string): Promise<InvitationResult>;
+  getCredentials(): Promise<Credential[]>;
+  getVersion(): Promise<Version>;
+}
+
+const rnONE: ONECore = NativeModules.ProcivisOneCoreModule;
+export default rnONE;
