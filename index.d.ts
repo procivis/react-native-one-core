99c39bfaed9af2e4350e7f659f4baa7ea1e8c46b
Developer <info@procivis.ch>
2023-07-27 15:54:04 +0200

    ONE-459: fixed typescript build

--- index.d.ts
+++ index.d.ts
@@ -1,79 +1,69 @@
 export interface Version {
-  target: string;
-  buildTime: string;
-  branch: string;
-  tag: string;
-  commit: string;
-  rustVersion: string;
-  pipelineId: string;
+    target: string;
+    buildTime: string;
+    branch: string;
+    tag: string;
+    commit: string;
+    rustVersion: string;
+    pipelineId: string;
 }
-
 export interface InvitationResult {
-  issuedCredentialId: string;
+    issuedCredentialId: string;
 }
-
-export enum CredentialState {
-  CREATED = "CREATED",
-  PENDING = "PENDING",
-  OFFERED = "OFFERED",
-  ACCEPTED = "ACCEPTED",
-  REJECTED = "REJECTED",
-  REVOKED = "REVOKED",
-  ERROR = "ERROR",
+export declare enum CredentialState {
+    CREATED = "CREATED",
+    PENDING = "PENDING",
+    OFFERED = "OFFERED",
+    ACCEPTED = "ACCEPTED",
+    REJECTED = "REJECTED",
+    REVOKED = "REVOKED",
+    ERROR = "ERROR"
 }
-
-export enum RevocationMethod {
-  NONE = "NONE",
-  STATUS_LIST2021 = "STATUS_LIST2021",
-  LVVC = "LVVC",
+export declare enum RevocationMethod {
+    NONE = "NONE",
+    STATUS_LIST2021 = "STATUS_LIST2021",
+    LVVC = "LVVC"
 }
-
-export enum CredentialFormat {
-  JWT = "JWT",
-  SD_JWT = "SD_JWT",
-  JSON_LD = "JSON_LD",
-  MDOC = "MDOC",
+export declare enum CredentialFormat {
+    JWT = "JWT",
+    SD_JWT = "SD_JWT",
+    JSON_LD = "JSON_LD",
+    MDOC = "MDOC"
 }
-
-export enum ClaimDataType {
-  STRING = "STRING",
-  DATE = "DATE",
-  NUMBER = "NUMBER",
+export declare enum ClaimDataType {
+    STRING = "STRING",
+    DATE = "DATE",
+    NUMBER = "NUMBER"
 }
-
 export interface CredentialSchema {
-  id: string;
-  createdDate: string;
-  lastModified: string;
-  name: string;
-  organisationId: string;
-  format: CredentialFormat;
-  revocationMethod: RevocationMethod;
+    id: string;
+    createdDate: string;
+    lastModified: string;
+    name: string;
+    organisationId: string;
+    format: CredentialFormat;
+    revocationMethod: RevocationMethod;
 }
-
 export interface Claim {
-  id: string;
-  key: string;
-  dataType: ClaimDataType;
-  value: string;
+    id: string;
+    key: string;
+    dataType: ClaimDataType;
+    value: string;
 }
-
 export interface Credential {
-  id: string;
-  createdDate: string;
-  issuanceDate: string;
-  lastModified: string;
-  issuerDid?: string | null;
-  state: CredentialState;
-  claims: Claim[];
-  schema: CredentialSchema;
+    id: string;
+    createdDate: string;
+    issuanceDate: string;
+    lastModified: string;
+    issuerDid?: string | null;
+    state: CredentialState;
+    claims: Claim[];
+    schema: CredentialSchema;
 }
-
 interface ONECore {
-  handleInvitation(url: string): Promise<InvitationResult>;
-  getCredentials(): Promise<Credential[]>;
-  getVersion(): Promise<Version>;
+    handleInvitation(url: string): Promise<InvitationResult>;
+    getCredentials(): Promise<Credential[]>;
+    getVersion(): Promise<Version>;
 }
-
 declare const rnONE: ONECore;
 export default rnONE;
