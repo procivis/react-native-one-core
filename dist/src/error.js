d92868c2d5fb52b953c94d613582bb1f70f78dd9
Developer <info@procivis.ch>
2024-10-01 21:53:25 +0200

    * ts split

---
+++ dist/src/error.js
@@ -0,0 +1,25 @@
+export var OneErrorCode;
+(function (OneErrorCode) {
+    // BindingError
+    OneErrorCode["AlreadyExists"] = "AlreadyExists";
+    OneErrorCode["NotFound"] = "NotFound";
+    OneErrorCode["NotSupported"] = "NotSupported";
+    OneErrorCode["ValidationError"] = "ValidationError";
+    OneErrorCode["ConfigValidationError"] = "ConfigValidationError";
+    OneErrorCode["Uninitialized"] = "Uninitialized";
+    OneErrorCode["DbErr"] = "DbErr";
+    OneErrorCode["IOError"] = "IOError";
+    OneErrorCode["Unknown"] = "Unknown";
+})(OneErrorCode || (OneErrorCode = {}));
+/**
+ * Specific errors being throw from the {@link ONECore} functions
+ */
+export class OneError extends Error {
+    code;
+    cause;
+    constructor(params) {
+        super(params.message);
+        this.code = params.code;
+        this.cause = params.cause;
+    }
+}
---
+++ src/trust.ts
@@ -0,0 +1,53 @@
+import { ListQuery, SortDirection } from "./list";
+
+export enum TrustAnchorRoleEnum {
+  PUBLISHER = "PUBLISHER",
+  CLIENT = "CLIENT",
+}
+
+export interface CreateTrustAnchorRequest {
+  name: string;
+  type: string;
+  publisherReference?: string;
+  role: TrustAnchorRoleEnum;
+  priority?: number;
+  organisationId: string;
+}
+
+export interface TrustAnchor {
+  id: string;
+  createdDate: string;
+  lastModified: string;
+  name: string;
+  type: string;
+  publisherReference?: string;
+  role: TrustAnchorRoleEnum;
+  priority?: number;
+  organisationId: string;
+}
+
+export interface TrustAnchorListItem extends TrustAnchor {
+  entities: number;
+}
+
+export enum SortableTrustAnchorColumnEnum {
+  NAME = "NAME",
+  CREATED_DATE = "CREATED_DATE",
+  TYPE = "TYPE",
+  ROLE = "ROLE",
+  PRIORITY = "PRIORITY",
+}
+
+export enum ExactTrustAnchorFilterColumnEnum {
+  NAME = "NAME",
+  TYPE = "TYPE",
+}
+
+export interface TrustAnchorListQuery extends ListQuery {
+  sort?: SortableTrustAnchorColumnEnum;
+  sortDirection?: SortDirection;
+  name?: string;
+  exact?: ExactTrustAnchorFilterColumnEnum[];
+  type?: string;
+  role?: TrustAnchorRoleEnum;
+}
