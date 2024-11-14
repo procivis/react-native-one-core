315cb39c69179cd344229f76083a114f363ddd5f
Developer <info@procivis.ch>
2024-11-14 16:03:57 +0100

    ONE-3876: fixed blocking of JS thread on android

--- android/src/main/java/ch/procivis/one/core/Util.kt
+++ android/src/main/java/ch/procivis/one/core/Util.kt
@@ -4,17 +4,21 @@ import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReadableMap
 
 object Util {
-    // error handling
+    // error handling + running core code outside JS thread
     fun asyncCall(promise: Promise, function: () -> Any?) {
-        try {
-            promise.resolve(function())
-        } catch (error: Throwable) {
-            promise.reject(
-                error.javaClass.simpleName.replaceFirst(Regex("Exception$"), "Error"),
-                error.message,
-                error
-            )
-        }
+        object : Thread() {
+            override fun run() {
+                try {
+                    promise.resolve(function())
+                } catch (error: Throwable) {
+                    promise.reject(
+                        error.javaClass.simpleName.replaceFirst(Regex("Exception$"), "Error"),
+                        error.message,
+                        error
+                    )
+                }
+            }
+        }.start()
     }
 }
 
