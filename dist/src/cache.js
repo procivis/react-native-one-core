02a89128105d459785072f1e9b553d12a753ed45
Developer <info@procivis.ch>
2025-01-08 15:03:40 +0100

    Auto SDK: Update Core bindings main

---
+++ dist/src/cache.js
@@ -0,0 +1,9 @@
+export var CacheType;
+(function (CacheType) {
+    CacheType["DID_DOCUMENT"] = "DID_DOCUMENT";
+    CacheType["JSON_LD_CONTEXT"] = "JSON_LD_CONTEXT";
+    CacheType["STATUS_LIST_CREDENTIAL"] = "STATUS_LIST_CREDENTIAL";
+    CacheType["VCT_METADATA"] = "VCT_METADATA";
+    CacheType["JSON_SCHEMA"] = "JSON_SCHEMA";
+    CacheType["TRUST_LIST"] = "TRUST_LIST";
+})(CacheType || (CacheType = {}));
