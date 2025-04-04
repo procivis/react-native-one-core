58ca59cd4ecdfa4593fa03019c868547eff2f039
Developer <info@procivis.ch>
2025-04-04 08:55:20 +0000

    Update core bindings from pipeline 1.38852.0

--- example/App.tsx
+++ example/App.tsx
@@ -18,16 +18,8 @@ const config = {
     },
     UBIQU_RSE: { disabled: false },
   },
-  exchange: {
-    OPENID4VC: {
-      params: {
-        private: {
-          encryption:
-            "93d9182795f0d1bec61329fc2d18c4b4c1b7e65e69e20ec30a2101a9875fff7e",
-        },
-      },
-    },
-    MDOC_OPENID4VP: {
+  issuanceProtocol: {
+    OPENID4VCI_DRAFT13: {
       params: {
         private: {
           encryption:
